e0986f9db3dca5bae55c0f04acc56cb587d5f326
MD. Saharior Ridoy <emonhridoy2014@gmail.com>
2025-01-23 00:44:20 +0600

    Added Contact us section into home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { AuthContext } from '../../Provider/AuthProvider';
 import Loading from '../../components/Loading/Loading';
 import Banner from '../Home/Banner/Banner.jsx'
+import ContactUs from './ContactUs/ContactUs.jsx';
 const Home = () => {
     const { user,loading } = useContext(AuthContext);
     console.log(user);
@@ -9,8 +10,11 @@ const Home = () => {
         return <Loading />
       }
     return (
-        <div className='w-[1320px] mx-auto'>
+        <div className='max-w-[1320px] mx-auto'>
             <Banner></Banner>
+            <div className='py-16'>
+            <ContactUs></ContactUs>
+            </div>
         </div>
     );
 };
