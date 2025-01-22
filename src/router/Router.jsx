662ebaabdcb5b1af1e2b87734fd1a50e7dd69440
MD. Saharior Ridoy <emonhridoy2014@gmail.com>
2025-01-22 22:21:15 +0600

    Added Error Page

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -6,6 +6,7 @@ import MainLayout from "../MainLayout/MainLayout";
 import Login from "../Pages/Login/Login";
 import Home from "../Pages/Home/Home";
 import Register from "../Pages/Register/Register";
+import Error from "../components/ErrorPage/Error";
 const router = createBrowserRouter([
     {
       path: "/",
@@ -25,7 +26,11 @@ const router = createBrowserRouter([
   {
       path:'/register',
       element:<Register></Register>
-  }
+  },
+  {
+    path: "*",
+    element: <Error></Error>,
+  },
 
   ]);
 
