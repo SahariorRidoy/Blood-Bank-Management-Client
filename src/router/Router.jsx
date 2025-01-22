6a367096c0de087cf870b52ded5d9d597fb9aec1
MD. Saharior Ridoy <emonhridoy2014@gmail.com>
2025-01-22 22:25:44 +0600

    Added Private Route

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,37 +1,38 @@
-import {
-    createBrowserRouter,
-    
-  } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../MainLayout/MainLayout";
 import Login from "../Pages/Login/Login";
 import Home from "../Pages/Home/Home";
 import Register from "../Pages/Register/Register";
 import Error from "../components/ErrorPage/Error";
+import Dashboard from "../Pages/Dashboard/Dashboard";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <MainLayout></MainLayout>,
-      children:[
-        {
-            path: "/",
-            element: <Home></Home>,
-          },
-       
-      ]
-    },
-    {
-      path:'/login',
-      element:<Login></Login>
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
   },
   {
-      path:'/register',
-      element:<Register></Register>
+    path: "/dashboard",
+    element: <PrivateRoute><Dashboard></Dashboard></PrivateRoute>,
+  },
+  {
+    path: "/login",
+    element: <Login></Login>,
+  },
+  {
+    path: "/register",
+    element: <Register></Register>,
   },
   {
     path: "*",
     element: <Error></Error>,
   },
+]);
 
-  ]);
-
-  export default router;
+export default router;
